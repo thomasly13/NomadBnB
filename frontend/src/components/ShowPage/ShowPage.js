5d1f7d0ec53165b454bd6c82a41069ad6200bbc4
Thomas Ly <Thomaslyy13@gmail.com>
2023-06-21 18:15:54 -0700

    fixed production bug

--- frontend/src/components/ShowPage/ShowPage.js
+++ frontend/src/components/ShowPage/ShowPage.js
@@ -16,7 +16,7 @@ export const ListingShow = () => {
 
     useEffect(() => {
         dispatch(fetchListingDetail(listingId))
-    }, [dispatch, listing])
+    }, [dispatch, state.listing[listingId]])
 
 
     return (
