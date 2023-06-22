af6ed07bfb41196d315f602172072db18ae0325c
Thomas Ly <Thomaslyy13@gmail.com>
2023-06-21 18:21:17 -0700

    debugged deployment bug

--- frontend/src/components/ShowPage/ShowPage.js
+++ frontend/src/components/ShowPage/ShowPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch} from "react-redux";
 import { fetchListingDetail } from "../../store/listing";
 import { useParams } from "react-router-dom";
