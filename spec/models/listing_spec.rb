3feaf079c40019edb6652ec97d940e3a43312693
Thomas Ly <Thomaslyy13@gmail.com>
2023-10-11 19:18:13 -0700

    added association test

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -36,5 +36,10 @@ RSpec.describe Listing, type: :model do
         it { should validate_presence_of(:owner_id)}
     end
 
+    describe 'associations' do 
+        it {should belong_to(:owner).class_name(:User)}
+    end
+
+    
     
 end
